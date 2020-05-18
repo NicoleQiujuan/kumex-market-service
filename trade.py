75ca01e96aeec71001ad8912106f4c4b2eb97b27
1bazinga25 <zyh_xg@163.com>
2020-05-18 11:10:28 +0800

    1

--- trade.py
+++ trade.py
@@ -85,11 +85,11 @@ class Kumex(object):
         best_flag = 0
         try:
             t = self.market.get_ticker(self.kumex_symbol)
-            # logging.info(t)
+            logging.info(t)
             best_bid_size = t['bestBidSize']
-            best_bid_price = t['bestBidPrice']
+            # best_bid_price = t['bestBidPrice']
             best_ask_size = t['bestAskSize']
-            best_ask_price = t['bestAskPrice']
+            # best_ask_price = t['bestAskPrice']
             max_size = 100000
             if best_bid_size > max_size:
                 best_bid_size = max_size
@@ -99,18 +99,19 @@ class Kumex(object):
         except Exception as e:
             best_flag = 0
             logging.error(e)
+            time.sleep(5)
 
         if best_flag > 0:
             try:
-                sell = self.trade.create_limit_order(self.kumex_symbol, 'sell', '5', best_ask_size, best_ask_price)
-                logging.info('在合约 %s 以数量= %s, 价格= %s, 吃卖单,订单ID = %s' %
-                             (self.kumex_symbol, best_ask_size, best_ask_price, sell['orderId']))
+                sell = self.trade.create_market_order(self.kumex_symbol, 'sell', '5', size=best_ask_size, type='market')
+                logging.info('在合约 %s 以数量= %s, 吃卖单,订单ID = %s' %
+                             (self.kumex_symbol, best_ask_size, sell['orderId']))
             except Exception as e:
                 logging.error(e)
             try:
-                buy = self.trade.create_limit_order(self.kumex_symbol, 'buy', '5', best_bid_size, best_bid_price)
-                logging.info('在合约 %s 以数量= %s, 价格= %s, 吃买单,订单ID = %s' %
-                             (self.kumex_symbol, best_bid_size, best_bid_price, buy['orderId']))
+                buy = self.trade.create_market_order(self.kumex_symbol, 'buy', '5', size=best_bid_size, type='market')
+                logging.info('在合约 %s 以数量= %s, 吃买单,订单ID = %s' %
+                             (self.kumex_symbol, best_bid_size, buy['orderId']))
             except Exception as e:
                 logging.error(e)
 
@@ -118,8 +119,8 @@ class Kumex(object):
         try:
             m = random.randint(self.sizeMin, self.sizeMax)
             ask = self.trade.create_limit_order(self.kumex_symbol, 'sell', '5', m, p)
-            # logging.info('当前盘口价格 = %s,在合约 %s 以数量= %s,价格= %s,创建了卖单,卖单ID = %s' %
-            #              (self.market_price, self.kumex_symbol, m, p, ask['orderId']))
+            logging.info('当前盘口价格 = %s,在合约 %s 以数量= %s,价格= %s,创建了卖单,卖单ID = %s' %
+                         (self.market_price, self.kumex_symbol, m, p, ask['orderId']))
             self.sell_list[p] = {
                 'price': p,
                 'side': 'sell',
@@ -133,8 +134,8 @@ class Kumex(object):
         try:
             m = random.randint(self.sizeMin, self.sizeMax)
             bid = self.trade.create_limit_order(self.kumex_symbol, 'buy', '5', m, p)
-            # logging.info('当前盘口价格 = %s,在合约 %s 以数量= %s,价格= %s,创建了买单,卖单ID = %s' %
-            #              (self.market_price, self.kumex_symbol, m, p, bid['orderId']))
+            logging.info('当前盘口价格 = %s,在合约 %s 以数量= %s,价格= %s,创建了买单,卖单ID = %s' %
+                         (self.market_price, self.kumex_symbol, m, p, bid['orderId']))
             self.sell_list[p] = {
                 'price': p,
                 'side': 'buy',
@@ -147,7 +148,7 @@ class Kumex(object):
     def cancel_order(self, order_id, key):
         try:
             self.trade.cancel_order(order_id)
-            # logging.info('当前盘口价 = %s,撤单 id = %s, key = %s' % (self.market_price, order_id, key))
+            logging.info('当前盘口价 = %s,撤单 id = %s, key = %s' % (self.market_price, order_id, key))
         except Exception as e:
             logging.info('order_id = %s, key = %s' % (order_id, key))
             logging.error(e)
@@ -162,7 +163,7 @@ class Kumex(object):
 
     def get_active_orders(self):
         try:
-            o = self.trade.get_order_list(symbol=self.kumex_symbol, status='active')
+            o = self.trade.get_order_list(symbol=self.kumex_symbol, status='active', type='limit')
             os = o['items']
         except Exception as e:
             logging.error(e)
@@ -194,86 +195,83 @@ if __name__ == '__main__':
     logging.info('Service Start ......')
     service = Kumex()
     while 1:
-        if service.side == 'taker':
-            service.taker()
-            time.sleep(0.5)
-        else:
-            service.get_market_price()
-            service.get_active_orders()
-            # break
-            # logging.info('sell maker 1 len(service.sell_list) = %s' % len(service.sell_list))
-            # logging.info(service.sell_list)
-            # 价格浮动
-            rand = random.randint(-20, 20)
-            # service.market_price += rand
-            ask_rand = service.market_price + 20
-            for k, v in list(service.sell_list.items()):
-                # 判断范围，不在范围内的单撤掉
-                if k not in range(service.market_price, ask_rand) and k in service.sell_list.keys():
-                    # logging.info('k not in range(service.market_price = %s' % k)
-                    service.cancel_order(v['order_id'], k)
-                    del service.sell_list[k]
-            # logging.info('sell maker 2 len(service.sell_list) = %s' % len(service.sell_list))
-            # logging.info(service.sell_list)
+        service.get_market_price()
+        service.taker()
+        service.get_active_orders()
+        # break
+        logging.info('sell maker 1 len(service.sell_list) = %s' % len(service.sell_list))
+        logging.info(service.sell_list)
+        # 价格浮动
+        # rand = random.randint(-20, 20)
+        # service.market_price += rand
+        ask_rand = service.market_price + 25
+        for k, v in list(service.sell_list.items()):
+            # 判断范围，不在范围内的单撤掉
+            if k not in range(service.market_price, ask_rand) and k in service.sell_list.keys():
+                # logging.info('k not in range(service.market_price = %s' % k)
+                service.cancel_order(v['order_id'], k)
+                del service.sell_list[k]
+        logging.info('sell maker 2 len(service.sell_list) = %s' % len(service.sell_list))
+        logging.info(service.sell_list)
 
-            for i in range(1, service.maker_number):
-                ask_price = service.market_price + i
-                # logging.info('for loop ask_price = %s' % ask_price)
-                if ask_price in service.sell_list.keys():
-                    # logging.info('ask_price in keys loop = %s' % ask_price)
-                    orderId = service.sell_list[ask_price]['order_id']
-                    order_info = service.get_order_info(orderId)
-                    if order_info:
-                        if not order_info['isActive']:
-                            # logging.info('not order_info[isActive] = %s' % ask_price)
-                            service.cancel_order(orderId, ask_price)
-                            del service.sell_list[ask_price]
+        for i in range(1, service.maker_number):
+            ask_price = service.market_price + i
+            # logging.info('for loop ask_price = %s' % ask_price)
+            if ask_price in service.sell_list.keys():
+                # logging.info('ask_price in keys loop = %s' % ask_price)
+                orderId = service.sell_list[ask_price]['order_id']
+                order_info = service.get_order_info(orderId)
+                if order_info:
+                    if not order_info['isActive']:
+                        # logging.info('not order_info[isActive] = %s' % ask_price)
+                        service.cancel_order(orderId, ask_price)
+                        del service.sell_list[ask_price]
+                        service.ask_maker(ask_price)
+                    else:
+                        size_dff = order_info['size'] - order_info['dealSize']
+                        if size_dff not in range(service.sizeMin, service.sizeMax):
+                            if order_info['isActive']:
+                                # logging.info('del service.sell_list[ask_price] = %s' % ask_price)
+                                service.cancel_order(order_info['id'], ask_price)
+                                del service.sell_list[ask_price]
+
                             service.ask_maker(ask_price)
-                        else:
-                            size_dff = order_info['size'] - order_info['dealSize']
-                            if size_dff not in range(service.sizeMin, service.sizeMax):
-                                if order_info['isActive']:
-                                    # logging.info('del service.sell_list[ask_price] = %s' % ask_price)
-                                    service.cancel_order(order_info['id'], ask_price)
-                                    del service.sell_list[ask_price]
 
-                                service.ask_maker(ask_price)
+            else:
+                # logging.info('target=service.ask_maker, = %s' % ask_price)
+                service.ask_maker(ask_price)
+        logging.info('sell maker 3 len(service.sell_list) = %s' % len(service.sell_list))
+        logging.info(service.sell_list)
 
-                else:
-                    # logging.info('target=service.ask_maker, = %s' % ask_price)
-                    service.ask_maker(ask_price)
-            # logging.info('sell maker 3 len(service.sell_list) = %s' % len(service.sell_list))
-            # logging.info(service.sell_list)
+        logging.info('buy maker 1 len(service.buy_list) = %s' % len(service.buy_list))
+        logging.info(service.buy_list)
+        bid_rand = service.market_price - 25
+        for k, v in list(service.buy_list.items()):
+            # 判断范围，不在范围内的单撤掉
+            if k not in range(bid_rand, service.market_price):
+                service.cancel_order(v['order_id'], k)
+                del service.buy_list[k]
+        logging.info('buy maker 2 len(service.buy_list) = %s' % len(service.buy_list))
+        logging.info(service.buy_list)
+        for i in range(1, service.maker_number):
+            bid_price = service.market_price - i + 1
+            if bid_price in service.buy_list.keys():
+                orderId = service.buy_list[bid_price]['order_id']
+                order_info = service.get_order_info(orderId)
+                if order_info:
+                    if not order_info['isActive']:
+                        service.cancel_order(orderId, bid_price)
+                        del service.buy_list[bid_price]
+                        service.bid_maker(bid_price)
+                    else:
+                        size_dff = order_info['size'] - order_info['dealSize']
+                        if size_dff not in range(service.sizeMin, service.sizeMax):
+                            if order_info['isActive']:
+                                service.cancel_order(order_info['id'], bid_price)
+                                del service.buy_list[bid_price]
 
-            # logging.info('buy maker 1 len(service.buy_list) = %s' % len(service.buy_list))
-            # logging.info(service.buy_list)
-            bid_rand = service.market_price - 20
-            for k, v in list(service.buy_list.items()):
-                # 判断范围，不在范围内的单撤掉
-                if k not in range(bid_rand, service.market_price):
-                    service.cancel_order(v['order_id'], k)
-                    del service.buy_list[k]
-            # logging.info('buy maker 2 len(service.buy_list) = %s' % len(service.buy_list))
-            # logging.info(service.buy_list)
-            for i in range(1, service.maker_number):
-                bid_price = service.market_price - i + 1
-                if bid_price in service.buy_list.keys():
-                    orderId = service.buy_list[bid_price]['order_id']
-                    order_info = service.get_order_info(orderId)
-                    if order_info:
-                        if not order_info['isActive']:
-                            service.cancel_order(orderId, bid_price)
-                            del service.buy_list[bid_price]
                             service.bid_maker(bid_price)
-                        else:
-                            size_dff = order_info['size'] - order_info['dealSize']
-                            if size_dff not in range(service.sizeMin, service.sizeMax):
-                                if order_info['isActive']:
-                                    service.cancel_order(order_info['id'], bid_price)
-                                    del service.buy_list[bid_price]
-
-                                service.bid_maker(bid_price)
-                else:
-                    service.bid_maker(bid_price)
-            # logging.info('buy maker 3 len(service.buy_list) = %s' % len(service.buy_list))
-            # logging.info(service.buy_list)
+            else:
+                service.bid_maker(bid_price)
+        logging.info('buy maker 3 len(service.buy_list) = %s' % len(service.buy_list))
+        logging.info(service.buy_list)
